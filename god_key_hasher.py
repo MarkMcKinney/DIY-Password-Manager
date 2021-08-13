fc699953928c236f743e4b325e6cc68ad9da18c6
Deep Bhatt <deep.bhatt@uxgorilla.com>
2021-08-13 23:25:36 +0530

    apply 'black' python linter

--- god_key_hasher.py
+++ god_key_hasher.py
@@ -9,39 +9,44 @@ import getpass
 
 # OPERATION FUNCTIONS
 
+
 def encrypt_data(input, hashed_pass):
     message = input.encode()
     f = Fernet(hashed_pass)
     encrypted = f.encrypt(message)
-    return (encrypted)
+    return encrypted
+
 
 def decrypt_data(input, hashed_pass):
     f = Fernet(hashed_pass)
     decrypted = f.decrypt(input)
-    return (decrypted)
+    return decrypted
+
 
 password_provided = getpass.getpass("What would you like your master password to be? ")
-password = password_provided.encode() # Convert to type bytes
-salt = os.urandom(random.randint(16,256))
+password = password_provided.encode()  # Convert to type bytes
+salt = os.urandom(random.randint(16, 256))
 kdf = PBKDF2HMAC(
     algorithm=hashes.SHA256(),
     length=32,
     salt=salt,
     iterations=100000,
-    backend=default_backend()
+    backend=default_backend(),
 )
-hashed_entered_pass = base64.urlsafe_b64encode(kdf.derive(password)) # Can only use kdf once
+hashed_entered_pass = base64.urlsafe_b64encode(
+    kdf.derive(password)
+)  # Can only use kdf once
 
 file = open("SALT.txt", "wb")
 file.write(salt)
 file.close()
 
 file = open("VERIFIER.txt", "wb")
-file.write(encrypt_data("entered_master_correct",hashed_entered_pass))
+file.write(encrypt_data("entered_master_correct", hashed_entered_pass))
 file.close()
 
 file = open("pm_db.mmf", "w+")
-file.write(str(encrypt_data("{}",hashed_entered_pass).decode('utf-8')))
+file.write(str(encrypt_data("{}", hashed_entered_pass).decode("utf-8")))
 file.close()
 
 print("Your password vault was created. Access it using the pm_db.py file.")
